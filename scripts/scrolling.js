fcb35514522bc64e358f63cb1bff86cfa51f6e8f
Julian Gamez <gamez.julian@gmail.com>
2018-02-08 12:44:00 -0300

    scrolling

---
+++ scripts/scrolling.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('.navbar ul li a[href^="#"]').on('click', function (e) {
+
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // store hash
+        var hash = this.hash;
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function () {
+
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+    });
+
+})(jQuery); // End of use strict
