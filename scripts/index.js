438af5c58fbfa2e74b0e726b05052c07215e5d11
Julian Gamez <gamez.julian@hotmail.com>
2018-02-07 21:12:50 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+var animatedHeader = (function () {
+    var shrinkHeader = 300;
+    $(window).scroll(function () {
+        var scroll = getCurrentScroll();
+        if (scroll >= shrinkHeader) {
+            $('.header').addClass('shrink');
+        }
+        else {
+            $('.header').removeClass('shrink');
+        }
+    });
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    }
+});
+
+var resizeHomeSection = (function () {
+    $(window).on("load resize", function () {
+        $(".container_banner .fill-screen-section").css("height", window.innerHeight);
+    });
+});
+
+
+$(function () {
+    animatedHeader();
+    resizeHomeSection();
+
+    $('.ihelpyou').scrollspy({
+        target: '.navbar'       
+    });
+});
